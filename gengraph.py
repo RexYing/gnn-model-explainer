a0941fdf54341c3891740a29e8d4125eda59a4e6
Dylan Bourgeois <dtsbourg@gmail.com>
2019-10-21 21:20:13 -0700

    formatting

--- gengraph.py
+++ gengraph.py
@@ -9,8 +9,9 @@ from matplotlib import pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib.colors as colors
+
 # Set matplotlib backend to file writing
-plt.switch_backend('agg')
+plt.switch_backend("agg")
 
 import networkx as nx
 
@@ -29,20 +30,20 @@ import utils.io_utils as io_utils
 #
 ####################################
 def perturb(graph_list, p):
-    ''' Perturb the list of graphs by adding/removing edges.
+    """ Perturb the list of graphs by adding/removing edges.
     Args:
         p_add: probability of adding edges. If None, estimate it according to graph density,
             such that the expected number of added edges is equal to that of deleted edges.
         p_del: probability of removing edges.
     Returns:
         A list of graphs that are perturbed from the original graphs.
-    '''
+    """
     perturbed_graph_list = []
     for G_original in graph_list:
         G = G_original.copy()
         # TODO: why are the edge removal commented?
-        #edge_remove_count = 0
-        #for (u, v) in list(G.edges()):
+        # edge_remove_count = 0
+        # for (u, v) in list(G.edges()):
         #    if np.random.rand()<p:
         #        G.remove_edge(u, v)
         #        edge_remove_count += 1
@@ -52,22 +53,22 @@ def perturb(graph_list, p):
             while True:
                 u = np.random.randint(0, G.number_of_nodes())
                 v = np.random.randint(0, G.number_of_nodes())
-                if (not G.has_edge(u,v)) and (u!=v):
+                if (not G.has_edge(u, v)) and (u != v):
                     break
             G.add_edge(u, v)
         perturbed_graph_list.append(G)
     return perturbed_graph_list
 
-   
+
 def join_graph(G1, G2, n_pert_edges):
-    ''' Join two graphs along matching nodes, then perturb the resulting graph.
+    """ Join two graphs along matching nodes, then perturb the resulting graph.
     Args:
         G1, G2: Networkx graphs to be joined.
         n_pert_edges: number of perturbed edges.
     Returns:
         A new graph, result of merging and perturbing G1 and G2.
-    '''
-    assert(n_pert_edges > 0)
+    """
+    assert n_pert_edges > 0
     F = nx.compose(G1, G2)
     edge_cnt = 0
     while edge_cnt < n_pert_edges:
@@ -75,44 +76,43 @@ def join_graph(G1, G2, n_pert_edges):
         node_2 = np.random.choice(G2.nodes())
         F.add_edge(node_1, node_2)
         edge_cnt += 1
-    return F 
+    return F
 
 
 def preprocess_input_graph(G, labels, normalize_adj=False):
-    ''' Load an existing graph to be converted for the experiments.
+    """ Load an existing graph to be converted for the experiments.
     Args:
         G: Networkx graph to be loaded.
         labels: Associated node labels.
         normalize_adj: Should the method return a normalized adjacency matrix.
     Returns:
         A dictionary containing adjacency, node features and labels
-    '''
+    """
     adj = np.array(nx.to_numpy_matrix(G))
     if normalize_adj:
         sqrt_deg = np.diag(1.0 / np.sqrt(np.sum(adj, axis=0, dtype=float).squeeze()))
         adj = np.matmul(np.matmul(sqrt_deg, adj), sqrt_deg)
 
-
     existing_node = list(G.nodes)[-1]
-    feat_dim = G.nodes[existing_node]['feat'].shape[0]
+    feat_dim = G.nodes[existing_node]["feat"].shape[0]
     f = np.zeros((G.number_of_nodes(), feat_dim), dtype=float)
     for i, u in enumerate(G.nodes()):
-        f[i, :] = G.nodes[u]['feat']
+        f[i, :] = G.nodes[u]["feat"]
 
     # add batch dim
     adj = np.expand_dims(adj, axis=0)
     f = np.expand_dims(f, axis=0)
     labels = np.expand_dims(labels, axis=0)
-    return {'adj': adj, 'feat': f, 'labels': labels}
+    return {"adj": adj, "feat": f, "labels": labels}
 
 
 ####################################
-# 
+#
 # Generating synthetic graphs
 #
 ###################################
-def gen_syn1(nb_shapes = 80, width_basis = 300, feature_generator = None, m = 5):
-    ''' Synthetic Graph #1:
+def gen_syn1(nb_shapes=80, width_basis=300, feature_generator=None, m=5):
+    """ Synthetic Graph #1:
 
     Start with Barabasi-Albert graph and attach house-shaped subgraphs.
 
@@ -126,31 +126,27 @@ def gen_syn1(nb_shapes = 80, width_basis = 300, feature_generator = None, m = 5)
         G                 :  A networkx graph
         role_id           : TODO
         name              : A graph identifier
-    '''
-    basis_type = 'ba'
-    list_shapes = [['house']] * nb_shapes
+    """
+    basis_type = "ba"
+    list_shapes = [["house"]] * nb_shapes
 
-    plt.figure(figsize=(8,6), dpi=300)
+    plt.figure(figsize=(8, 6), dpi=300)
 
     G, role_id, _ = synthetic_structsim.build_graph(
-                                                    width_basis, 
-                                                    basis_type, 
-                                                    list_shapes,
-                                                    start=0, 
-                                                    m=5
-                                                    )
+        width_basis, basis_type, list_shapes, start=0, m=5
+    )
     G = perturb([G], 0.01)[0]
 
     if feature_generator is None:
         feature_generator = featgen.ConstFeatureGen(1)
     feature_generator.gen_node_features(G)
 
-    name = basis_type + '_' + str(width_basis) + '_' + str(nb_shapes)
+    name = basis_type + "_" + str(width_basis) + "_" + str(nb_shapes)
     return G, role_id, name
 
 
-def gen_syn2(nb_shapes = 100, width_basis = 350):
-    ''' Synthetic Graph #2:
+def gen_syn2(nb_shapes=100, width_basis=350):
+    """ Synthetic Graph #2:
 
     Start with Barabasi-Albert graph and add node features indicative of a community label.
 
@@ -162,15 +158,15 @@ def gen_syn2(nb_shapes = 100, width_basis = 350):
         G                 :  A networkx graph
         label             : TODO
         name              : A graph identifier
-    '''
-    basis_type = 'ba'
+    """
+    basis_type = "ba"
 
     random_mu = [0.0] * 8
     random_sigma = [1.0] * 8
 
     # Create two grids
-    mu_1, sigma_1 = np.array([-1.0]*2 + random_mu), np.array([0.5]*2 + random_sigma)
-    mu_2, sigma_2 = np.array([1.0]*2 + random_mu), np.array([0.5]*2 + random_sigma)
+    mu_1, sigma_1 = np.array([-1.0] * 2 + random_mu), np.array([0.5] * 2 + random_sigma)
+    mu_2, sigma_2 = np.array([1.0] * 2 + random_mu), np.array([0.5] * 2 + random_sigma)
     feat_gen_G1 = featgen.GaussianFeatureGen(mu=mu_1, sigma=sigma_1)
     feat_gen_G2 = featgen.GaussianFeatureGen(mu=mu_2, sigma=sigma_2)
     G1, role_id1, name = gen_syn1(feature_generator=feat_gen_G1, m=4)
@@ -179,23 +175,24 @@ def gen_syn2(nb_shapes = 100, width_basis = 350):
     num_roles = max(role_id1) + 1
     role_id2 = [r + num_roles for r in role_id2]
     label = role_id1 + role_id2
-    
+
     # Edit node ids to avoid collisions on join
-    g1_map = {n:i for i,n in enumerate(G1.nodes())}
+    g1_map = {n: i for i, n in enumerate(G1.nodes())}
     G1 = nx.relabel_nodes(G1, g1_map)
-    g2_map = {n:i+G1_size for i,n in enumerate(G2.nodes())}
+    g2_map = {n: i + G1_size for i, n in enumerate(G2.nodes())}
     G2 = nx.relabel_nodes(G2, g2_map)
 
     # Join
     n_pert_edges = width_basis
     G = join_graph(G1, G2, n_pert_edges)
-   
-    name = basis_type + '_' + str(width_basis) + '_' + str(nb_shapes) + '_2comm'
+
+    name = basis_type + "_" + str(width_basis) + "_" + str(nb_shapes) + "_2comm"
 
     return G, label, name
 
-def gen_syn3(nb_shapes = 80, width_basis = 300, feature_generator = None, m = 5):
-    ''' Synthetic Graph #3:
+
+def gen_syn3(nb_shapes=80, width_basis=300, feature_generator=None, m=5):
+    """ Synthetic Graph #3:
 
     Start with Barabasi-Albert graph and attach cycle-shaped subgraphs.
 
@@ -209,30 +206,27 @@ def gen_syn3(nb_shapes = 80, width_basis = 300, feature_generator = None, m = 5)
         G                 :  A networkx graph
         role_id           : TODO
         name              : A graph identifier
-    '''
-    basis_type = 'ba'
-    list_shapes = [['cycle', 6]] * nb_shapes
+    """
+    basis_type = "ba"
+    list_shapes = [["cycle", 6]] * nb_shapes
 
-    plt.figure(figsize=(8,6), dpi=300)
+    plt.figure(figsize=(8, 6), dpi=300)
 
     G, role_id, _ = synthetic_structsim.build_graph(
-                        width_basis, 
-                        basis_type, 
-                        list_shapes,
-                        start=0, 
-                        m=5)
+        width_basis, basis_type, list_shapes, start=0, m=5
+    )
     G = perturb([G], 0.01)[0]
 
     if feature_generator is None:
         feature_generator = featgen.ConstFeatureGen(1)
     feature_generator.gen_node_features(G)
 
-    name = basis_type + '_' + str(width_basis) + '_' + str(nb_shapes)
+    name = basis_type + "_" + str(width_basis) + "_" + str(nb_shapes)
     return G, role_id, name
 
 
-def gen_syn4(nb_shapes = 60, width_basis = 8, feature_generator = None, m = 4):
-    ''' Synthetic Graph #4:
+def gen_syn4(nb_shapes=60, width_basis=8, feature_generator=None, m=4):
+    """ Synthetic Graph #4:
 
     Start with a tree and attach cycle-shaped subgraphs.
 
@@ -246,30 +240,32 @@ def gen_syn4(nb_shapes = 60, width_basis = 8, feature_generator = None, m = 4):
         G                 :  A networkx graph
         role_id           :  TODO
         name              :  A graph identifier
-    '''
-    basis_type = 'tree'
-    list_shapes = [['cycle', 6]] * nb_shapes
+    """
+    basis_type = "tree"
+    list_shapes = [["cycle", 6]] * nb_shapes
 
-    fig = plt.figure(figsize=(8,6), dpi=300)
+    fig = plt.figure(figsize=(8, 6), dpi=300)
 
-    G, role_id, plugins = synthetic_structsim.build_graph(width_basis, basis_type, list_shapes,
-            start=0)
+    G, role_id, plugins = synthetic_structsim.build_graph(
+        width_basis, basis_type, list_shapes, start=0
+    )
     G = perturb([G], 0.01)[0]
 
     if feature_generator is None:
         feature_generator = featgen.ConstFeatureGen(1)
     feature_generator.gen_node_features(G)
 
-    name = basis_type + '_' + str(width_basis) + '_' + str(nb_shapes)
+    name = basis_type + "_" + str(width_basis) + "_" + str(nb_shapes)
 
-    path = os.path.join('log/syn4_base_h20_o20')
+    path = os.path.join("log/syn4_base_h20_o20")
     writer = SummaryWriter(path)
-    io_utils.log_graph(writer, G, 'graph/full')
+    io_utils.log_graph(writer, G, "graph/full")
 
     return G, role_id, name
 
-def gen_syn5(nb_shapes = 80, width_basis = 8, feature_generator=None, m=3):
-    ''' Synthetic Graph #5:
+
+def gen_syn5(nb_shapes=80, width_basis=8, feature_generator=None, m=3):
+    """ Synthetic Graph #5:
 
     Start with a tree and attach grid-shaped subgraphs.
 
@@ -283,26 +279,24 @@ def gen_syn5(nb_shapes = 80, width_basis = 8, feature_generator=None, m=3):
         G                 :  A networkx graph
         role_id           :  TODO
         name              :  A graph identifier
-    '''
-    basis_type  = 'tree'
-    list_shapes = [['grid', m]] * nb_shapes
+    """
+    basis_type = "tree"
+    list_shapes = [["grid", m]] * nb_shapes
 
-    plt.figure(figsize=(8,6), dpi=300)
+    plt.figure(figsize=(8, 6), dpi=300)
 
     G, role_id, _ = synthetic_structsim.build_graph(
-                            width_basis, 
-                            basis_type, 
-                            list_shapes,
-                            start=0)
+        width_basis, basis_type, list_shapes, start=0
+    )
     G = perturb([G], 0.1)[0]
 
     if feature_generator is None:
         feature_generator = featgen.ConstFeatureGen(1)
     feature_generator.gen_node_features(G)
 
-    name = basis_type + '_' + str(width_basis) + '_' + str(nb_shapes)
+    name = basis_type + "_" + str(width_basis) + "_" + str(nb_shapes)
 
-    path = os.path.join('log/syn5_base_h20_o20')
+    path = os.path.join("log/syn5_base_h20_o20")
     writer = SummaryWriter(path)
 
     return G, role_id, name
