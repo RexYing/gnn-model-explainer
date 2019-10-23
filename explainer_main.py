11220399f21408eff6a29c4b6d28a2bb12269cd7
Dylan Bourgeois <dtsbourg@gmail.com>
2019-10-22 20:12:43 -0700

    begin refactoring of explainer_main

--- explainer_main.py
+++ explainer_main.py
@@ -1,4 +1,3 @@
-
 import sklearn.metrics as metrics
 from tensorboardX import SummaryWriter
 
@@ -12,115 +11,167 @@ import torch
 import utils.io_utils as io_utils
 import utils.parser_utils as parser_utils
 import models
-from explainer import  explain
+from explainer import explain
 
 
 def arg_parse():
-    parser = argparse.ArgumentParser(description='GraphPool arguments.')
+    parser = argparse.ArgumentParser(description="GraphPool arguments.")
     io_parser = parser.add_mutually_exclusive_group(required=False)
-    io_parser.add_argument('--dataset', dest='dataset',
-            help='Input dataset.')
+    io_parser.add_argument("--dataset", dest="dataset", help="Input dataset.")
     benchmark_parser = io_parser.add_argument_group()
-    benchmark_parser.add_argument('--bmname', dest='bmname',
-            help='Name of the benchmark dataset')
-    io_parser.add_argument('--pkl', dest='pkl_fname',
-            help='Name of the pkl data file')
+    benchmark_parser.add_argument(
+        "--bmname", dest="bmname", help="Name of the benchmark dataset"
+    )
+    io_parser.add_argument("--pkl", dest="pkl_fname", help="Name of the pkl data file")
 
     parser_utils.parse_optimizer(parser)
 
-    parser.add_argument('--logdir', dest='logdir',
-            help='Tensorboard log directory')
-    parser.add_argument('--ckptdir', dest='ckptdir',
-            help='Model checkpoint directory')
-    parser.add_argument('--cuda', dest='cuda',
-            help='CUDA.')
-    parser.add_argument('--gpu', dest='gpu', action='store_const',
-            const=True, default=False,
-            help='whether to use GPU.')
-    parser.add_argument('--epochs', dest='num_epochs', type=int,
-            help='Number of epochs to train.')
-    parser.add_argument('--hidden-dim', dest='hidden_dim', type=int,
-            help='Hidden dimension')
-    parser.add_argument('--output-dim', dest='output_dim', type=int,
-            help='Output dimension')
-    parser.add_argument('--num-gc-layers', dest='num_gc_layers', type=int,
-            help='Number of graph convolution layers before each pooling')
-    parser.add_argument('--bn', dest='bn', action='store_const',
-            const=True, default=False,
-            help='Whether batch normalization is used')
-    parser.add_argument('--dropout', dest='dropout', type=float,
-            help='Dropout rate.')
-    parser.add_argument('--nobias', dest='bias', action='store_const',
-            const=False, default=True,
-            help='Whether to add bias. Default to True.')
-    parser.add_argument('--no-writer', dest='writer', action='store_const',
-            const=False, default=True,
-            help='Whether to add bias. Default to True.')
+    parser.add_argument("--logdir", dest="logdir", help="Tensorboard log directory")
+    parser.add_argument("--ckptdir", dest="ckptdir", help="Model checkpoint directory")
+    parser.add_argument("--cuda", dest="cuda", help="CUDA.")
+    parser.add_argument(
+        "--gpu",
+        dest="gpu",
+        action="store_const",
+        const=True,
+        default=False,
+        help="whether to use GPU.",
+    )
+    parser.add_argument(
+        "--epochs", dest="num_epochs", type=int, help="Number of epochs to train."
+    )
+    parser.add_argument(
+        "--hidden-dim", dest="hidden_dim", type=int, help="Hidden dimension"
+    )
+    parser.add_argument(
+        "--output-dim", dest="output_dim", type=int, help="Output dimension"
+    )
+    parser.add_argument(
+        "--num-gc-layers",
+        dest="num_gc_layers",
+        type=int,
+        help="Number of graph convolution layers before each pooling",
+    )
+    parser.add_argument(
+        "--bn",
+        dest="bn",
+        action="store_const",
+        const=True,
+        default=False,
+        help="Whether batch normalization is used",
+    )
+    parser.add_argument("--dropout", dest="dropout", type=float, help="Dropout rate.")
+    parser.add_argument(
+        "--nobias",
+        dest="bias",
+        action="store_const",
+        const=False,
+        default=True,
+        help="Whether to add bias. Default to True.",
+    )
+    parser.add_argument(
+        "--no-writer",
+        dest="writer",
+        action="store_const",
+        const=False,
+        default=True,
+        help="Whether to add bias. Default to True.",
+    )
     # Explainer
-    parser.add_argument('--mask-act', dest='mask_act', type=str,
-            help='sigmoid, ReLU.')
-    parser.add_argument('--mask-bias', dest='mask_bias', action='store_const',
-            const=True, default=False,
-            help='Whether to add bias. Default to True.')
-    parser.add_argument('--explain-node', dest='explain_node', type=int,
-            help='Node to explain.')
-    parser.add_argument('--graph-idx', dest='graph_idx', type=int,
-            help='Graph to explain.')
-    parser.add_argument('--graph-mode', dest='graph_mode', action='store_const',
-            const=True, default=False,
-            help='whether to run Explainer on Graph Classification task.')
-    parser.add_argument('--multigraph-class', dest='multigraph_class', type=int,
-            help='whether to run Explainer on multiple Graphs from the Classification task for examples in the same class.')
-    parser.add_argument('--multinode-class', dest='multinode_class', type=int,
-            help='whether to run Explainer on multiple nodes from the Classification task for examples in the same class.')
-    parser.add_argument('--align-steps', dest='align_steps', type=int,
-            help='Number of iterations to find P, the alignment matrix.')
+    parser.add_argument("--mask-act", dest="mask_act", type=str, help="sigmoid, ReLU.")
+    parser.add_argument(
+        "--mask-bias",
+        dest="mask_bias",
+        action="store_const",
+        const=True,
+        default=False,
+        help="Whether to add bias. Default to True.",
+    )
+    parser.add_argument(
+        "--explain-node", dest="explain_node", type=int, help="Node to explain."
+    )
+    parser.add_argument(
+        "--graph-idx", dest="graph_idx", type=int, help="Graph to explain."
+    )
+    parser.add_argument(
+        "--graph-mode",
+        dest="graph_mode",
+        action="store_const",
+        const=True,
+        default=False,
+        help="whether to run Explainer on Graph Classification task.",
+    )
+    parser.add_argument(
+        "--multigraph-class",
+        dest="multigraph_class",
+        type=int,
+        help="whether to run Explainer on multiple Graphs from the Classification task for examples in the same class.",
+    )
+    parser.add_argument(
+        "--multinode-class",
+        dest="multinode_class",
+        type=int,
+        help="whether to run Explainer on multiple nodes from the Classification task for examples in the same class.",
+    )
+    parser.add_argument(
+        "--align-steps",
+        dest="align_steps",
+        type=int,
+        help="Number of iterations to find P, the alignment matrix.",
+    )
 
-    parser.add_argument('--method', dest='method', type=str,
-            help='Method. Possible values: base, att')
-    parser.add_argument('--name-suffix', dest='name_suffix',
-            help='suffix added to the output filename')
-    parser.add_argument('--explainer-suffix', dest='explainer_suffix',
-            help='suffix added to the explainer log')
+    parser.add_argument(
+        "--method", dest="method", type=str, help="Method. Possible values: base, att"
+    )
+    parser.add_argument(
+        "--name-suffix", dest="name_suffix", help="suffix added to the output filename"
+    )
+    parser.add_argument(
+        "--explainer-suffix",
+        dest="explainer_suffix",
+        help="suffix added to the explainer log",
+    )
 
-    parser.set_defaults(logdir='log',
-                        ckptdir='ckpt',
-                        dataset='syn1',
-                        opt='adam',   # opt_parser
-                        opt_scheduler='none',
-                        cuda='0',
-                        lr=0.1,
-                        clip=2.0,
-                        batch_size=20,
-                        num_epochs=500,
-                        hidden_dim=20,
-                        output_dim=20,
-                        num_gc_layers=3,
-                        dropout=0.0,
-                        method='base',
-                        name_suffix='',
-                        explainer_suffix='',
-                        align_steps=1000,
-                        explain_node=None,
-                        graph_idx=-1,
-                        mask_act='sigmoid',
-                        multigraph_class=-1,
-                        multinode_class=-1
-                       )
+    parser.set_defaults(
+        logdir="log",
+        ckptdir="ckpt",
+        dataset="syn1",
+        opt="adam",  
+        opt_scheduler="none",
+        cuda="0",
+        lr=0.1,
+        clip=2.0,
+        batch_size=20,
+        num_epochs=500,
+        hidden_dim=20,
+        output_dim=20,
+        num_gc_layers=3,
+        dropout=0.0,
+        method="base",
+        name_suffix="",
+        explainer_suffix="",
+        align_steps=1000,
+        explain_node=None,
+        graph_idx=-1,
+        mask_act="sigmoid",
+        multigraph_class=-1,
+        multinode_class=-1,
+    )
     return parser.parse_args()
+
 
 def main():
     prog_args = arg_parse()
 
     if prog_args.gpu:
-        os.environ['CUDA_VISIBLE_DEVICES'] = prog_args.cuda
-        print('CUDA', prog_args.cuda)
+        os.environ["CUDA_VISIBLE_DEVICES"] = prog_args.cuda
+        print("CUDA", prog_args.cuda)
     else:
-        print('Using CPU')
+        print("Using CPU")
 
     if prog_args.writer:
         path = os.path.join(prog_args.logdir, io_utils.gen_explainer_prefix(prog_args))
-        #if os.path.isdir(path):
+        # if os.path.isdir(path):
         #    print('Remove existing log dir: ', path)
         #    shutil.rmtree(path)
         writer = SummaryWriter(path)
@@ -128,60 +179,96 @@ def main():
         writer = None
 
     ckpt = io_utils.load_ckpt(prog_args)
-    cg_dict = ckpt['cg']
-    input_dim = cg_dict['feat'].shape[2]
-    num_classes = cg_dict['pred'].shape[2]
-    print('input dim: ', input_dim, '; num classes: ', num_classes)
+    cg_dict = ckpt["cg"]
+    input_dim = cg_dict["feat"].shape[2]
+    num_classes = cg_dict["pred"].shape[2]
+    print("input dim: ", input_dim, "; num classes: ", num_classes)
 
-    graph_mode = prog_args.graph_mode or prog_args.multigraph_class >= 0 or prog_args.graph_idx >= 0
+    graph_mode = (
+        prog_args.graph_mode
+        or prog_args.multigraph_class >= 0
+        or prog_args.graph_idx >= 0
+    )
 
     # build model
-    print('Method: ', prog_args.method)
+    print("Method: ", prog_args.method)
     if graph_mode:
-      model = models.GcnEncoderGraph(input_dim, prog_args.hidden_dim, prog_args.output_dim, num_classes,
-                                   prog_args.num_gc_layers, bn=prog_args.bn, args=prog_args)
+        model = models.GcnEncoderGraph(
+            input_dim,
+            prog_args.hidden_dim,
+            prog_args.output_dim,
+            num_classes,
+            prog_args.num_gc_layers,
+            bn=prog_args.bn,
+            args=prog_args,
+        )
     else:
-      if prog_args.dataset == 'ppi_essential':
-          prog_args.loss_weight = torch.tensor([1, 5.], dtype=torch.float).cuda()
-      model = models.GcnEncoderNode(input_dim, prog_args.hidden_dim, prog_args.output_dim, num_classes,
-                                   prog_args.num_gc_layers, bn=prog_args.bn, args=prog_args,
-                                   )
+        if prog_args.dataset == "ppi_essential":
+            prog_args.loss_weight = torch.tensor([1, 5.0], dtype=torch.float).cuda()
+        model = models.GcnEncoderNode(
+            input_dim,
+            prog_args.hidden_dim,
+            prog_args.output_dim,
+            num_classes,
+            prog_args.num_gc_layers,
+            bn=prog_args.bn,
+            args=prog_args,
+        )
     if prog_args.gpu:
         model = model.cuda()
-    model.load_state_dict(ckpt['model_state'])
+    model.load_state_dict(ckpt["model_state"])
 
-    explainer = explain.Explainer(model, cg_dict['adj'], cg_dict['feat'],
-                                  cg_dict['label'], cg_dict['pred'], cg_dict['train_idx'],
-                                  prog_args, writer=writer, print_training=True, graph_mode=graph_mode, 
-                                  graph_idx=prog_args.graph_idx)
-    train_idx = cg_dict['train_idx']
+    explainer = explain.Explainer(
+        model,
+        cg_dict["adj"],
+        cg_dict["feat"],
+        cg_dict["label"],
+        cg_dict["pred"],
+        cg_dict["train_idx"],
+        prog_args,
+        writer=writer,
+        print_training=True,
+        graph_mode=graph_mode,
+        graph_idx=prog_args.graph_idx,
+    )
+    train_idx = cg_dict["train_idx"]
     if prog_args.explain_node is not None:
         explainer.explain(prog_args.explain_node, unconstrained=False)
     elif graph_mode:
         if prog_args.multigraph_class >= 0:
-            print(cg_dict['label'])
+            print(cg_dict["label"])
             # only run for graphs with label specified by multigraph_class
-            labels = cg_dict['label'].numpy()
+            labels = cg_dict["label"].numpy()
             graph_indices = []
             for i, l in enumerate(labels):
                 if l == prog_args.multigraph_class:
                     graph_indices.append(i)
                 if len(graph_indices) > 30:
                     break
-            print('Graph indices for label ', prog_args.multigraph_class, ' : ', graph_indices)
+            print(
+                "Graph indices for label ",
+                prog_args.multigraph_class,
+                " : ",
+                graph_indices,
+            )
             explainer.explain_graphs(graph_indices=graph_indices)
-                
+
         elif prog_args.graph_idx == -1:
             # just run for a customized set of indices
-            explainer.explain_graphs(graph_indices=[1,2,3,4])
+            explainer.explain_graphs(graph_indices=[1, 2, 3, 4])
         else:
-            explainer.explain(node_idx=0, graph_idx=prog_args.graph_idx, graph_mode=True, unconstrained=False)
-            io_utils.plot_cmap_tb(writer, 'tab20', 20, 'tab20_cmap')
+            explainer.explain(
+                node_idx=0,
+                graph_idx=prog_args.graph_idx,
+                graph_mode=True,
+                unconstrained=False,
+            )
+            io_utils.plot_cmap_tb(writer, "tab20", 20, "tab20_cmap")
     else:
         if prog_args.multinode_class >= 0:
-            print(cg_dict['label'])
+            print(cg_dict["label"])
             # only run for nodes with label specified by multinode_class
-            labels = cg_dict['label'][0]  # already numpy matrix
+            labels = cg_dict["label"][0]  # already numpy matrix
 
             node_indices = []
             for i, l in enumerate(labels):
@@ -189,7 +276,12 @@ def main():
                     break
                 if l == prog_args.multinode_class:
                     node_indices.append(i)
-            print('Node indices for label ', prog_args.multinode_class, ' : ', node_indices)
+            print(
+                "Node indices for label ",
+                prog_args.multinode_class,
+                " : ",
+                node_indices,
+            )
             explainer.explain_nodes(node_indices, prog_args)
 
         else:
@@ -197,17 +289,18 @@ def main():
             # masked_adj = explainer.explain_nodes([370,390], prog_args)
             # masked_adj = explainer.explain_nodes_gnn_cluster([370,390], prog_args)
             # masked_adj = explainer.explain_nodes_gnn_cluster(range(400, 700, 5), prog_args)
-            masked_adj = explainer.explain_nodes_gnn_stats(range(400, 700, 5), prog_args)
-            #pickle.dump(masked_adj, open('out/masked_adjs.pkl', 'wb'))
+            masked_adj = explainer.explain_nodes_gnn_stats(
+                range(400, 700, 5), prog_args
+            )
+            # pickle.dump(masked_adj, open('out/masked_adjs.pkl', 'wb'))
         # explain a set of nodes
         # masked_adj = explainer.explain_nodes([370,390], prog_args)
         # masked_adj = explainer.explain_nodes_gnn_cluster([370,390], prog_args)
         # masked_adj = explainer.explain_nodes_gnn_cluster(range(400, 700, 5), prog_args)
         # masked_adj = explainer.explain_nodes_gnn_stats(range(511, 811, 6), prog_args)
-        #pickle.dump(masked_adj, open('out/masked_adjs.pkl', 'wb'))
+        # pickle.dump(masked_adj, open('out/masked_adjs.pkl', 'wb'))
 
 
-        
 if __name__ == "__main__":
     main()
 
