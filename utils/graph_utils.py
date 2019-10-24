37337b1da6ad459a9d846cd59e99b1f9dc717c9e
Dylan Bourgeois <dtsbourg@gmail.com>
2019-10-23 18:31:07 -0700

    move neihborhood computation to graph_utils

--- utils/graph_utils.py
+++ utils/graph_utils.py
@@ -144,3 +144,15 @@ class GraphSampler(torch.utils.data.Dataset):
             "assign_feats": self.assign_feat_all[idx].copy(),
         }
 
+def neighborhoods(adj, n_hops, use_cuda):
+    """Returns the n_hops degree adjacency matrix adj."""
+    adj = torch.tensor(adj, dtype=torch.float)
+    if use_cuda:
+        adj = adj.cuda()
+    hop_adj = power_adj = adj
+    for i in range(n_hops - 1):
+        power_adj = power_adj @ adj
+        prev_hop_adj = hop_adj
+        hop_adj = hop_adj + power_adj
+        hop_adj = (hop_adj > 0).float()
+    return hop_adj.cpu().numpy().astype(int)
